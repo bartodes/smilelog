e79cb28d76aa7d814c58f2e28318bcd7bbd6bf97
bartodes <bartojdes@gmail.com>
2026-03-21 03:57:23 +0000

    feat: structuring project

---
+++ internals/models/patient.go
@@ -0,0 +1,12 @@
+package models
+
+type Patient struct {
+	Name        string
+	Id          string
+	Email       string
+	PhoneNumber uint
+}
+
+type Appointment struct{}
+
+type Visit struct{}
--- models.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Patient struct{}
-
-type Appointment struct{}
-
-type Visit struct{}
