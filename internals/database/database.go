160dc179ca21d2b39a216d063a4e4e4411f84abf
bartodes <bartojdes@gmail.com>
2026-03-25 00:00:18 +0000

    feat: created database initialization process

--- internals/database/database.go
+++ internals/database/database.go
@@ -4,22 +4,81 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const dbName string = "smilelog"
+const DB_NAME string = "smilelog"
 
-func OpenDb() {
-	db, err := sql.Open("sqlite3", fmt.Sprintf("./%s.db", dbName))
+func InitDb() {
+	db_path := getDbPath()
+	db := openDb(db_path)
+	defer db.Close()
+
+	ok := tableExists(db, "patients")
+
+	if !ok {
+		execDbSchema(db)
+	}
+}
+
+func getDbPath() string {
+	base_path, err := os.Getwd()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer db.Close()
+	return fmt.Sprintf("%s/%s.db", base_path, DB_NAME)
+}
 
-	_, err = db.Exec(tableSchema)
+func openDb(db_path string) *sql.DB {
+	db, err := sql.Open("sqlite3", db_path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+func tableExists(db *sql.DB, table_name string) bool {
+	query := "SELECT count(*) FROM sqlite_master WHERE type='table' AND name=?"
+
+	row := db.QueryRow(query, table_name)
+
+	var count int
+	err := row.Scan(&count)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return count > 0
+}
+
+func execDbSchema(db *sql.DB) {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer tx.Rollback()
+
+	for _, query := range tableSchema {
+		_, err := tx.Exec(query)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		log.Fatal(err)
 	}
--- internals/database/schema.go
+++ internals/database/schema.go
@@ -1,40 +1,35 @@
 package database
 
-// REMPLAZAR ENUM POR CHECK CONSTRAINT
-var tableSchema = `
-CREATE TABLE  IF NOT EXISTS "patients" (
-	"id" INTEGER PRIMARY KEY,
-	"name" TEXT NOT NULL,
-	"last_name" TEXT,
-	"email" TEXT UNIQUE,
-	"telephone_number" TEXT UNIQUE,
-	CONSTRAINT "check_null_email_or_telephone_number" CHECK (email IS NOT NULL OR telephone_number IS NOT NULL)
-);
-
-PRAGMA foreign_keys = ON;
-
-CREATE TABLE IF NOT EXISTS "appointments" (
-	"id" INTEGER PRIMARY KEY,
-	"patient_id" INTEGER NOT NULL,
-	"status" TEXT 
-		NOT NULL 
-		DEFAULT 'CREATED' 
-		CHECK (status in ('CREATED','CONFIRMED','CANCELED','NO_SHOW')),
-	"created_on" TEXT NOT NULL,
-	"scheduled_for" TEXT NOT NULL,
-	"duration" TEXT NOT NULL DEFAULT '1h',
-	FOREIGN KEY (patient_id) REFERENCES patients(id)
-);
-
-CREATE TABLE IF NOT EXISTS "visits" ( 
-	"id" INTEGER PRIMARY KEY,
-	"appointment_id" INTEGER UNIQUE NOT NULL,
-	"created_on" TEXT NOT NULL,
-	"notes" TEXT,
-	FOREIGN KEY (appointment_id) REFERENCES appointments(id)
-);
-
-CREATE INDEX IF NOT EXISTS "idx_appointment_scheduled" ON "appointments" ("scheduled_for");
-
-CREATE INDEX IF NOT EXISTS "idx_comp_appointment_patient" ON "appointments" ("patient_id", "scheduled_for");
-`
+// appointmentsStatus Enum as Check
+var tableSchema = []string{
+	`CREATE TABLE  IF NOT EXISTS "patients" (
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"name" TEXT NOT NULL,
+		"last_name" TEXT,
+		"email" TEXT UNIQUE,
+		"telephone_number" TEXT UNIQUE,
+		CONSTRAINT "check_null_email_or_telephone_number" CHECK (email IS NOT NULL OR telephone_number IS NOT NULL)
+	);`,
+	"PRAGMA foreign_keys = ON;",
+	`CREATE TABLE IF NOT EXISTS "appointments" (
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"patient_id" INTEGER NOT NULL,
+		"status" TEXT 
+			NOT NULL 
+			DEFAULT 'CREATED' 
+			CHECK (status in ('CREATED','CONFIRMED','CANCELED','NO_SHOW')),
+		"created_on" TEXT NOT NULL,
+		"scheduled_for" TEXT NOT NULL,
+		"duration" TEXT NOT NULL DEFAULT '1h',
+		FOREIGN KEY (patient_id) REFERENCES patients(id)
+	);`,
+	`CREATE TABLE IF NOT EXISTS "visits" ( 
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"appointment_id" INTEGER UNIQUE NOT NULL,
+		"created_on" TEXT NOT NULL,
+		"notes" TEXT,
+		FOREIGN KEY (appointment_id) REFERENCES appointments(id)
+	);`,
+	"CREATE INDEX IF NOT EXISTS 'idx_appointment_scheduled' ON 'appointments' ('scheduled_for');",
+	"CREATE INDEX IF NOT EXISTS 'idx_comp_appointment_patient' ON 'appointments' ('patient_id', 'scheduled_for');",
+}
