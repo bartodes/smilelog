5cf86dafe38c3c849a0e31a6f4fc2c5922a4e680
bartodes <bartojdes@gmail.com>
2026-03-26 02:27:37 +0000

    feat: CreatePatient func and patient model

--- internals/services/patient.go
+++ internals/services/patient.go
@@ -1,9 +1,46 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Patient struct {
-	Id       uint
-	Name     string
-	LastName string
-	Email    string
-	TelNum   uint
+	Id          uint
+	DocumentId  uint
+	Name        string
+	LastName    string
+	Email       string
+	PhoneNumber uint
 }
+
+func CreatePatient(p Patient, db *sql.DB) (Patient, error) {
+
+	query := `
+	INSERT INTO patients (document_id, name, last_name, email, phone_number) 
+	VALUES (?,?,?,?,?)
+	RETURNING id;`
+
+	res, err := db.Exec(
+		query,
+		p.DocumentId,
+		p.Name,
+		p.LastName,
+		p.Email,
+		p.PhoneNumber,
+	)
+	if err != nil {
+		return p, fmt.Errorf("insert error: %w", err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return p, fmt.Errorf("result error: %w", err)
+	}
+
+	p.Id = uint(id)
+
+	return p, nil
+}
+
+func (p Patient) GetPatientAppointments() {}
