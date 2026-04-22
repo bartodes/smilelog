3f9a8e44f466905fc3d42afc2f6005b5e2f4006f
bartodes <bartojdes@gmail.com>
2026-04-22 01:44:53 +0000

    feat: create appointment

--- cmd/appointment.go
+++ cmd/appointment.go
@@ -1,8 +1,16 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"time"
+
+	. "github.com/bartodes/smilelog/internals/models"
+	. "github.com/bartodes/smilelog/internals/services"
 	"github.com/spf13/cobra"
 )
+
+var appointment Appointment
 
 var appointmentCmd = &cobra.Command{
 	Use:   "appointment",
@@ -12,6 +20,19 @@ var appointmentCmd = &cobra.Command{
 var appointmentCreateCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create an appointment",
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := GetPatient(appointment.PatientID, db); err != nil {
+			log.Fatal(err)
+		}
+
+		appointment.ScheduledFor = time.Now().UTC()
+
+		a, err := CreateAppointment(appointment, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(a)
+	},
 }
 
 var appointmentListCmd = &cobra.Command{
@@ -35,4 +56,8 @@ func init() {
 	appointmentCmd.AddCommand(appointmentListCmd)
 	appointmentCmd.AddCommand(appointmentCancelCmd)
 	appointmentCmd.AddCommand(appointmentCompleteCmd)
+
+	appointmentCreateCmd.Flags().Int64VarP(&appointment.PatientID, "patient-id", "p", 0, "id of the patient")
+	// appointmentCreateCmd.Flags().TimeVarP(&appointment.ScheduledFor, "schedule", "s", , "duration of the appointment (in minutes)")
+	appointmentCreateCmd.Flags().IntVarP(&appointment.DurationMinutes, "duration", "d", 30, "duration of the appointment (in minutes)")
 }
