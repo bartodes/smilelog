5a29ae79525044a3054c4155d3015fad78b5ba37
bartodes <bartojdes@gmail.com>
2026-04-25 19:28:42 +0000

    chore: improved err msg

--- internals/services/errors.go
+++ internals/services/errors.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrAppointmentNotCreated = errors.New("appointment status is not 'CREATED'")
 	ErrAppointmentNotFound   = errors.New("appointment not found")
-	ErrAppointmentOverlap    = errors.New("detected schedule overlap")
+	ErrAppointmentOverlap    = errors.New("appointment schedule overlap")
 	ErrPatientNotFound       = errors.New("patient not found")
 )
